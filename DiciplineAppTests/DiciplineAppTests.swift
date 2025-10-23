f265b10430fd361c6f7286edb8dd6f95875a04d5
Omar <05-lynx-manikin@icloud.com>
2025-10-23 17:20:45 -0400

    Initial Commit

---
+++ DiciplineAppTests/DiciplineAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiciplineAppTests.swift
+//  DiciplineAppTests
+//
+//  Created by Omar Aldakheel on 23/10/2025.
+//
+
+import Testing
+@testable import DiciplineApp
+
+struct DiciplineAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
